5592fba2009314bdab8d1429eee1e9044fcd4ac1
Cheng Vue <cvu3@ucsc.edu>
2019-01-08 11:20:48 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,9 +11,10 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet])
-    if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+    @pet = Pet.new
+    @pet.create(params[:pet_name], params[:owner_name])
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
     end
     redirect to "pets/#{@pet.id}"
   end
